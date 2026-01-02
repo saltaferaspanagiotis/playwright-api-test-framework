1cd41ae7e520e7256dffc8f216d5260bc7c59aea
psaltafe <panagiotis.saltaferas@ext.euipo.europa.eu>
2026-01-02 23:09:56 +0200

    Removed obsolete log

--- environment-config/env.ts
+++ environment-config/env.ts
@@ -5,7 +5,6 @@ export const loadEnv = () => {
     if ( !process.env.NODE_ENV ) {
         process.env.NODE_ENV = 'local';
     }
-    console.log(`Using environment variables from testdata.${process.env.NODE_ENV}.env file`);
     const env = process.env.NODE_ENV;
     const envFilePath = resolve(__dirname, `../environment-config/testdata.${env}.env`);
     dotenv.config({ path: envFilePath });
